6707344683a1d3709d717a66530716e10c071735
Tanishq <tanishqbakka1@gmail.com>
2023-01-21 22:23:24 +0530

    update

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 console.log("------------------------------------\n[*]Server active at http://localhost:8080 \n------------------------------------");
-var express = require('express');
+var express = require ('express');
 const app = express();
-app.get('/:dt',function(req,res){
-  console.log("[*] request accepted");
-    // res.send("hello world"+req.params.dt);
-});
+app.get('/:type/:url', function(req,res){
+  console.log(req.params.type+" " +req.params.url);
+}
+);
 app.listen(8080);
-
-
